052cbf65c2dae44365c41aa2dbc6391b3f62ac55
kurlayusha <tbark.guard@gmail.com>
2022-12-17 15:32:25 +0300

    side bar items done

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,29 @@
 function SideBar() {
-  return <div className="sidebar">Sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="taskSide">
+        <h3>All Boards (3)</h3>
+        {/* TODO: we have to map all boards 
+        and take them titles then for each title there
+        shoul a button. */}
+        <button>Platform Launch</button>
+        <button>Marketing Plan</button>
+        <button>Roadmap</button>
+        <button>+Create New Board</button>
+      </div>
+      <footer className="sideBarSettingsSide">
+        <div className="toggle--light--dark">
+          {/* img */}
+          <button>toggle light-dark</button>
+          {/* img */}
+        </div>
+        <button>
+          {/* eye img */}
+          Hide Sidebar
+        </button>
+      </footer>
+    </div>
+  )
 }
 
 export default SideBar
