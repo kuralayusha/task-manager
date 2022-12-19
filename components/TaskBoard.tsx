1e10ebe223ad96232ee07034635cce47f655702a
kurlayusha <tbark.guard@gmail.com>
2022-12-19 17:32:01 +0300

    subtasks is now counted and displayed

--- components/TaskBoard.tsx
+++ components/TaskBoard.tsx
@@ -7,25 +7,31 @@ type TaskBoardProps = {
 }
 
 function TaskBoard({ tasksData, taskBoardFocus }: TaskBoardProps) {
+  const [subtasksCompleted, setSubtasksCompleted] = useState<any>({})
+  const [subtasksLength, setSubtasksLength] = useState<any>({})
+
+  console.log(subtasksCompleted, subtasksLength)
   return (
     <div className="task-board">
       {/* TODO: if there is no column show 
       a title and a button to add a new column */}
-      {/* TODO: if there is a column map them and print evry column and 
-      in every column print the tasks */}
       {tasksData.boards.map((board: any) => {
         if (board.name === taskBoardFocus) {
           return board.columns.map((column: any) => (
             <div key={column.name}>
               <h3>{column.name}</h3>
               {column.tasks.map((task: any) => (
-                <div key={task.title}>
+                <button key={task.title}>
                   <h4>{task.title}</h4>
-                  {/* todo: check the subtasks length, 
-                  isCompleted and 
-                  print it like "1 of 3 substask is done"  */}
-                  <p>{/* something */}</p>
-                </div>
+                  <p>
+                    {
+                      task.subtasks.filter(
+                        (subtask: any) => subtask.isCompleted === true
+                      ).length
+                    }
+                    of {task.subtasks.length} subtasks
+                  </p>
+                </button>
               ))}
             </div>
           ))
